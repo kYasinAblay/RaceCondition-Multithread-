360d07ec20d177411cd8c9f57de091a03d7427da
K. Yasin <58375357+kYasinAblay@users.noreply.github.com>
2021-12-31 10:03:33 +0300

    Add project files.

---
+++ RaceCondition(Multithread)/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int total = 0;
+Parallel.ForEach(Enumerable.Range(1, 100).ToList(), () => 0, (x, loop, subtotal) =>
+{
+    subtotal += x;
+    return subtotal;
+}, (y) => Interlocked.Add(ref total, y));
+
+Console.WriteLine(total);
+Console.WriteLine();
+Console.WriteLine();
+
+Parallel.For(0, 100, () => 0, (x, loop, subtotal) => { return subtotal += x; }, (y) => { Interlocked.Add(ref total, y); });
